8f8e732c6543e9388a89e2f749b71a5f67ae3511
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-06-26 00:32:00 +0300

    added animation

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,18 +1,22 @@
 import { useSelector } from 'react-redux';
 import { Task } from '../Task/Task';
 import { selectAllTasks } from 'redux/tasks/selectors';
-import css from './TaskList.module.css';
+// import css from './TaskList.module.css';
+import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export const TaskList = () => {
   const tasks = useSelector(selectAllTasks);
 
   return (
-    <ul className={css.list}>
-      {tasks.map(({ id, text }, index) => (
-        <li key={id}>
-          <Task id={id} text={text} numbers={index + 1} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <TransitionGroup>
+        {tasks.map(({ id, text }, index) => (
+          <CSSTransition key={id} timeout={500} classNames="post">
+            <Task id={id} text={text} numbers={index + 1} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </>
   );
 };
